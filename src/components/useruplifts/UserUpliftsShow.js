20a616925c7c81d9a0a58bc689d8095ad419e371
djc74 <davidcooper974@gmail.com>
2017-12-07 20:46:29 +0000

    delete button

--- src/components/useruplifts/UserUpliftsShow.js
+++ src/components/useruplifts/UserUpliftsShow.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Axios from 'axios';
 
+import BackButton from '../utilities/BackButton';
+
 class UserUpliftsShow extends React.Component {
   state = {
     useruplifts: {}
+  }
+
+  deleteUplift = () => {
+    Axios
+      .delete(`/api/useruplifts/${this.props.match.params.id}`)
+      .then(() => this.props.history.push('/useruplifts'))
+      .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -17,8 +26,10 @@ class UserUpliftsShow extends React.Component {
     return (
       <div>
         <h1>User Uplift show</h1>
+        <BackButton />
         <img src={this.state.useruplifts.body}/>
         <h3>{this.state.useruplifts.category}</h3>
+        <button onClick={this.deleteUplift}>Delete</button>
       </div>
     );
   }
