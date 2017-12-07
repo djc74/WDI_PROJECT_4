ff04172988a3a54135c6658e67f71adb1f322a61
djc74 <davidcooper974@gmail.com>
2017-12-07 14:24:12 +0000

    show route started

--- src/components/utilities/Routes.js
+++ src/components/utilities/Routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 
 import UpliftsIndex from '../uplifts/UpliftsIndex';
+import UpliftsShow from '../uplifts/UpliftsShow';
 
 const Routes = () => {
   return (
     <Switch>
+      <Route path="/uplifts/:id" component={UpliftsShow} />
       <Route exact path="/" component={UpliftsIndex} />
     </Switch>
   );
