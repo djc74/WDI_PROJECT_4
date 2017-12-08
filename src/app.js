3c22a8855224572c04e86f42f5b923d0efb2fe52
djc74 <davidcooper974@gmail.com>
2017-12-08 11:37:14 +0000

    login/register done

--- src/app.js
+++ src/app.js
@@ -4,16 +4,31 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 import './scss/style.scss';
 
+import Auth from './lib/Auth';
 import Routes from './components/utilities/Routes';
+
+
+function logout(e) {
+  e.preventDefault();
+  Auth.removeToken();
+  history.push('/');
+}
 
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <main>
-          <Routes />
-        </main>
+        <div>
+          <nav>
+            <button> <Link to="/register">Register</Link></button>
+            <button> <Link to="/login">Login</Link></button>
+            <button href="#" onClick={logout}>Logout</button>
+          </nav>
+          <main>
+            <Routes />
+          </main>
+        </div>
       </Router>
     );
   }
